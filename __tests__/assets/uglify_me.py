6cfc47a6ba4d10fd80485e13bf3de79e10262dcb
Harvey Rogers <harveyr@gmail.com>
2019-12-20 15:23:30 -0800

    mypy test working

--- __tests__/assets/uglify_me.py
+++
@@ -1,5 +0,0 @@
-# Make this file as ugly as you please (temporarily) to test the action.
-
-
-def thing(arg: str) -> bool:
-    return bool(arg)
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import * as core from '@actions/core'
-import * as path from 'path'
-
-const MATCHERS_PATH = path.join(__dirname, '..', '.github/matchers')
+import * as matchers from './matchers'
 
 async function run(): Promise<void> {
-  const matchers = ['mypy-with-code.json']
-
-  for (const matcher of matchers) {
-    const matcherPath = path.join(MATCHERS_PATH, matcher)
-    console.log(`##[add-matcher]${matcherPath}`)
-  }
+  matchers.installMatchers()
 }
 
 run().catch(err => {
